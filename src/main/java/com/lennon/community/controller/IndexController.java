f3fbf1b909a9ffe4396473f890b8c65a9a06865f
JFloyd <jiazekun008@gmail.com>
2021-06-24 18:24:04 +0800

    add cookies and session to keep login

--- src/main/java/com/lennon/community/controller/IndexController.java
+++ src/main/java/com/lennon/community/controller/IndexController.java
@@ -1,7 +1,13 @@
 package com.lennon.community.controller;
 
+import com.lennon.community.mapper.UserMapper;
+import com.lennon.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author jzk
@@ -9,8 +15,24 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/a")
-    public String index(){
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
         return "index";
     }
 
--- src/main/java/com/lennon/community/mapper/UserMapper.java
+++ src/main/java/com/lennon/community/mapper/UserMapper.java
@@ -3,14 +3,21 @@ package com.lennon.community.mapper;
 import com.lennon.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author jzk
  * @date 2021/6/24-16:53
  */
 @Mapper
+@Repository
 public interface UserMapper {
     @Insert("insert into user(name, account_id, token, gmt_create, gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token")String token);
 
 }
